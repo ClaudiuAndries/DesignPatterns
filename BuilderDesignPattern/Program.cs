7553ba5e589636de394718bd5ffc89db315421c3
Claudiu Andries <claudiu.andries99@gmail.com>
2024-01-18 22:10:19 +0200

    Add Builder and Factory method

--- BuilderDesignPattern/Program.cs
+++ BuilderDesignPattern/Program.cs
@@ -1,10 +1,96 @@
-﻿using System;
-namespace Builder;
+﻿namespace Builder;
+
+public class Pizza
+{
+    public string Dough, Sauce, Cheese, Topping, Size;
+
+    // address
+    public string StreetAddress, Postcode, City;
+
+    public override string ToString()
+    {
+        return $"{nameof(Dough)}: {Dough}, {nameof(Sauce)}: {Sauce}, {nameof(Cheese)}: {Cheese}, {nameof(Topping)}: {Topping}, {nameof(Size)}: {Size}, {nameof(StreetAddress)}: {StreetAddress}, " +
+               $"{nameof(Postcode)}: {Postcode}, {nameof(City)}: {City}";
+    }
+}
+
+public class PizzaBuilder // Facade
+{
+    protected Pizza pizza = new Pizza();
+
+    public PizzaIngredientsBuilder Ingredients => new PizzaIngredientsBuilder(pizza);
+    public PizzaAddressBuilder Address => new PizzaAddressBuilder(pizza);
+
+    public static implicit operator Pizza(PizzaBuilder pb) => pb.pizza; 
+}
+//INGREDIENTS
+public class PizzaIngredientsBuilder : PizzaBuilder
+{
+    public PizzaIngredientsBuilder(Pizza pizza)
+    {
+        this.pizza = pizza;
+    }
+    public PizzaIngredientsBuilder SetDough(string dough)
+    {
+        pizza.Dough = dough;
+        return this;
+    }
+    public PizzaIngredientsBuilder SetSauce(string sauce)
+    {
+        pizza.Sauce = sauce;
+        return this;
+    }
+    public PizzaIngredientsBuilder SetTopping(string topping)
+    {
+        pizza.Topping = topping;
+        return this;
+    }
+    public PizzaIngredientsBuilder SetSize(string size)
+    {
+        pizza.Size = size;
+        return this;
+    }
+}
+// Address
+public class PizzaAddressBuilder : PizzaBuilder
+{
+    public PizzaAddressBuilder(Pizza pizza)
+    {
+        this.pizza = pizza;
+    }
+    public PizzaAddressBuilder At(string streetAddress)
+    {
+        pizza.StreetAddress = streetAddress;
+        return this;
+    }
+    public PizzaAddressBuilder WithPostcode(string postcode)
+    {
+        pizza.Postcode = postcode;
+        return this;
+    }
+    public PizzaAddressBuilder AtCity(string city)
+    {
+        pizza.City = city;
+        return this;
+    }
+}
+
 
 class MainApp
 {
     static void Main()
     {
+        var pizzaBuilder = new PizzaBuilder();
+        Pizza pizza = pizzaBuilder.Ingredients
+                        .SetDough("Normal")
+                        .SetSauce("Ketchup")
+                        .SetSize("Large")
+                        .SetTopping("Test")
+                    .Address
+                        .At("Street Test")
+                        .AtCity("Iasi")
+                        .WithPostcode("A12 B345");
 
+        Console.WriteLine(pizza);
     }
 }
--- FactoryMethod/Program.cs
+++ FactoryMethod/Program.cs
@@ -1,10 +1,57 @@
 ﻿using System;
 namespace FactoryMethod;
 
-class MainApp
-{
-    static void Main()
-    {
 
+public class Point
+{
+    private double x, y;
+
+    protected Point(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    // factory property
+
+    public static Point Origin => new Point(0, 0);
+
+    // singleton field
+    public static Point Origin2 = new Point(0, 0);
+
+    // factory method
+
+    public static Point NewCartesianPoint(double x, double y)
+    {
+        return new Point(x, y);
+    }
+
+    public static Point NewPolarPoint(double rho, double theta)
+    {
+        return null;
+    }
+
+    // make it lazy
+    public static class Factory
+    {
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho * Math.Sin(theta), theta * Math.Cos(rho)); ;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var origin = Point.Origin;
+
+        var p2 = Point.Factory.NewCartesianPoint(1, 2);
     }
 }
