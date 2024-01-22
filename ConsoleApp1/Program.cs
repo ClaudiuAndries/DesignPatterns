67eb1b301a0a9892ae035303ca258fce5bb0ad45
Claudiu Andries <claudiu.andries99@gmail.com>
2024-01-22 15:04:57 +0200

    Add Adaptor

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,8 +3,45 @@ namespace Adapter;
 
 class MainApp
 {
-    static void Main()
+    public interface ITarget
     {
+        string GetRequest();
+    }
 
+    class Adaptee
+    {
+        public string GetSpecificRequest()
+        {
+            return "Specific request.";
+        }
+    }
+
+    class Adapter : ITarget
+    {
+        private readonly Adaptee _adaptee;
+
+        public Adapter(Adaptee adaptee)
+        {
+            this._adaptee = adaptee;
+        }
+
+        public string GetRequest()
+        {
+            return $"This is '{this._adaptee.GetSpecificRequest()}'";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adaptee adaptee = new Adaptee();
+            ITarget target = new Adapter(adaptee);
+
+            Console.WriteLine("Adaptee interface is incompatible with the client.");
+            Console.WriteLine("But with adapter client can call it's method.");
+
+            Console.WriteLine(target.GetRequest());
+        }
     }
 }
