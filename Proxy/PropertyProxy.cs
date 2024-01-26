daf8adfd49f1aa91603b49077456a35476d6be78
Claudiu Andries <claudiu.andries99@gmail.com>
2024-01-26 23:36:17 +0200

    Adding proxy and rename project to adaptor

---
+++ Proxy/PropertyProxy.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace DotNetDesignPatternDemos.Structural.Proxy
+{
+    class Creature
+    {
+        public byte Age = 0;
+        public int X = 0, Y = 0;
+    }
+
+    class Creatures
+    {
+        private readonly int size;
+        private byte[] age;
+        private int[] x, y;
+
+        public Creatures(int size)
+        {
+            this.size = size;
+            age = new byte[size];
+            x = new int[size];
+            y = new int[size];
+        }
+
+        public struct CreatureProxy
+        {
+            private readonly Creatures creatures;
+            private readonly int index;
+
+            public CreatureProxy(Creatures creatures, int index)
+            {
+                this.creatures = creatures;
+                this.index = index;
+            }
+
+            public ref byte Age => ref creatures.age[index];
+            public ref int X => ref creatures.x[index];
+            public ref int Y => ref creatures.y[index];
+        }
+
+        public IEnumerator<CreatureProxy> GetEnumerator()
+        {
+            for (int pos = 0; pos < size; ++pos)
+                yield return new CreatureProxy(this, pos);
+        }
+    }
+
+    public class SoACompositeProxy
+    {
+        public static void Main(string[] args)
+        {
+            var creatures = new Creature[100000000];
+            var creatures2 = new Creatures(100000000);
+
+            for (int i = 0; i < creatures.Length; i++)
+            {
+                creatures[i] = new Creature(); // Initialize each element with a new instance
+            }
+
+            foreach (var c in creatures)
+            {
+                c.X++; // not memory-efficient
+            }
+
+            foreach (var c in creatures2)
+            {
+                c.X++;
+            }
+        }
+    }
+}
---
+++ Proxy/ProtectedProxy.cs
@@ -0,0 +1,57 @@
+﻿//using System.Security.Cryptography.X509Certificates;
+
+//namespace ProtectionProxyDesignPattern;
+
+//public interface ICar
+//{
+//    void Drive();
+//}
+
+//public class Car : ICar
+//{
+//    public void Drive()
+//    {
+//        Console.WriteLine("You are driving");
+//    }
+//}
+
+//public class Driver
+//{
+//    public int Age { get; set; }
+
+//    public Driver(int age)
+//    {
+//        Age = age;
+//    }
+//}
+
+//public class CarProxi : ICar
+//{
+//    private Driver driver;
+//    private Car car = new Car();
+
+//    public CarProxi(Driver driver)
+//    {
+//        this.driver = driver;
+//    }
+
+//    public void Drive()
+//    {
+//        if (driver.Age >= 16)
+//        {
+//            car.Drive();
+//        }
+//        else
+//        {
+//            Console.WriteLine("too young");
+//        }
+//    }
+//}
+////internal class Program
+////{
+////    static void Main(string[] args)
+////    {
+////        ICar car = new CarProxi(new Driver(12));
+////        car.Drive();
+////    }
+////}
