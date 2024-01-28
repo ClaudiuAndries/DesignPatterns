c80ff40f6c05d01b79da4ba34b1baf96dbdfc2d8
Claudiu Andries <claudiu.andries99@gmail.com>
2024-01-28 21:12:08 +0200

    Adding Observer

---
+++ Observer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Channels;
+
+namespace ObserverDesignPattern;
+
+public class Person
+{
+    public event EventHandler<FallsIllEventArgs> FallsIll;
+
+    public void CatchACold()
+    {
+        FallsIll?.Invoke(this, new FallsIllEventArgs{ Address = "Test" });
+    }
+}
+
+public class  FallsIllEventArgs
+{
+    public string Address;
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var person = new Person();
+
+        person.FallsIll += CallDoctor;
+
+        person.CatchACold();
+
+        person.FallsIll -= CallDoctor;
+    }
+
+    private static void CallDoctor(object? sender, FallsIllEventArgs e)
+    {
+        Console.WriteLine($"A doctor has been called to {e.Address}");
+    }
+}
+ 
