27f4fa653bb0be5abb781530d2e8b94175876b0e
Claudiu Andries <claudiu.andries99@gmail.com>
2024-01-28 20:47:51 +0200

    Adding NullObject

---
+++ NullObject/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace NullObjectDesignPattern;
+
+public interface ILog
+{
+    void Info(string msg);
+    void Warn(string msg);
+}
+
+public class ConsoleLog : ILog
+{
+    public void Info(string msg)
+    {
+        Console.WriteLine(msg);
+    }
+
+    public void Warn(string msg)
+    {
+        Console.WriteLine($"WARNING!!!: {msg}");
+    }
+}
+
+
+public class BankAccount
+{
+    private ILog log;
+    private int balance;
+
+    public BankAccount(ILog log)
+    {
+        this.log = log;
+    }
+
+    public void Deposit(int amount)
+    {
+        balance += amount;
+        log.Info($"Deposited {amount}, balance is now {balance}");
+    }
+}
+
+public class NullLog : ILog
+{
+    public void Info(string msg)
+    {
+    }
+
+    public void Warn(string msg)
+    {
+    }
+}
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var bankAccount = new BankAccount(new NullLog());
+
+        bankAccount.Deposit(100);
+    }
+}
