7da5d439f489f9a0c2b07c00cb8d01a67c0add02
Claudiu Andries <claudiu.andries99@gmail.com>
2024-01-28 17:03:17 +0200

    Adding Iterator

---
+++ Iterator/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Iterator2DesignPattern;
+public class Node<T>
+{
+    public T Value;
+    public Node<T> Left, Right;
+    public Node<T> Parent;
+
+    public Node(T value)
+    {
+        Value = value;
+    }
+
+    public Node(T value, Node<T> left, Node<T> right)
+    {
+        Value = value;
+        Left = left;
+        Right = right;
+        left.Parent = right.Parent = this;
+    }
+
+}
+public class BinaryTree<T>
+{
+    private Node<T> root;
+
+    public BinaryTree(Node<T> root)
+    {
+        this.root = root;
+    }
+    public IEnumerable<Node<T>> InOrder
+    {
+        get
+        {
+            IEnumerable<Node<T>> Traverse(Node<T> current)
+            {
+                if (current.Left != null)
+                {
+                    foreach (var left in Traverse((current.Left)))
+                        yield return left;
+                }
+                yield return current;
+
+                if (current.Right != null)
+                {
+                    foreach (var right in Traverse((current.Right)))
+                        yield return right;
+                }
+            }
+
+            foreach (var node in Traverse(root))
+            {
+                yield return node;
+            }
+        }
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var root = new Node<int>(1, new Node<int>(2), new Node<int>(3));
+        var tree = new BinaryTree<int>(root);
+        Console.WriteLine(string.Join(",", tree.InOrder.Select(x => x.Value)));
+    }
+}
---
+++ Iterator/ProgramIteratorC++.cs
@@ -0,0 +1,89 @@
+﻿namespace IteratorDesignPattern;
+
+public class Node<T>
+{
+    public T Value;
+    public Node<T> Left, Right;
+    public Node<T> Parent;
+
+    public Node(T value)
+    {
+        Value = value;
+    }
+
+    public Node(T value, Node<T> left, Node<T> right)
+    {
+        Value = value;
+        Left = left;
+        Right = right;
+        left.Parent = right.Parent = this;
+    }
+
+}
+
+public class InOrderIterator<T>
+{
+    private readonly Node<T> _root;
+    public Node<T> Current;
+    private bool yieldStart;
+
+    public InOrderIterator(Node<T> root)
+    {
+        _root = root;
+        Current = root;
+        while(Current.Left != null)
+            Current = Current.Left;
+    }
+
+    public bool MoveNext()
+    {
+        if (!yieldStart)
+        {
+            yieldStart = true;
+            return true;
+        }
+
+        if (Current.Right != null)
+        {
+            Current = Current.Right;
+            while (Current.Left != null)
+            {
+                Current = Current.Left;
+            }
+
+            return true;
+        }
+        else
+        {
+            var p = Current.Parent;
+            while (p != null && Current == p.Right)
+            {
+                Current = p;
+                p = p.Parent;
+            }
+
+            Current = p;
+            return Current != null;
+        }
+    }
+
+    public void Reset()
+    {
+        Current = _root;
+        yieldStart = false;
+    }
+}
+
+internal class ProgramLikeInC
+{
+    static void MainC(string[] args)
+    {
+        var root = new Node<int>(1, new Node<int>(2), new Node<int>(3));
+        var it = new InOrderIterator<int>(root);
+        while (it.MoveNext())
+        {
+            Console.Write(it.Current.Value);
+            Console.Write(",");
+        }
+    }
+}
